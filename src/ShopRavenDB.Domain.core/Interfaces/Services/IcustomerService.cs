327515f7a73d79123a7002acd93c5c2674518561
IlkaCarmo <ilka.scarmo@gmail.com>
2025-01-25 22:41:22 -0300

    Conf

---
+++ src/ShopRavenDB.Domain.core/Interfaces/Services/IcustomerService.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopRavenDB.Domain.core.Interfaces.Services
+{
+    public interface IcustomerService
+    {
+        void AddCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(string id);
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomerById(string id);
+    }
+}
